405fb20e36b2f538abcaca656bb82f57ffeaf20f
Kenrick Hatton <Kenrick.hatton@gmail.com>
2018-02-18 11:43:23 +0000

    fixes #1: cheack if user has entered a valid direction

--- adventure/data.py
+++ adventure/data.py
@@ -1,6 +1,6 @@
 locations = {
-    (0, 0): 'House',
-    (0, 1): 'lake',
-    (1, 0): 'park',
-    (1, 1): 'market',
+    (0, 0): 'House',    # north
+    (0, 1): 'lake',     # south
+    (1, 0): 'park',     # east
+    (1, 1): 'market',   # west
 }
